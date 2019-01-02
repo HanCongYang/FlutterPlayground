2957e351735073db7055603df945d745c50f7f72
hancongyang <warship495949598@vip.qq.com>
2019-01-02 09:48:44 +0800

    no message

---
+++ hello_flutter/lib/constrant.dart
@@ -0,0 +1,8 @@
+class AppColors {
+  static const AppBarColor = 0xff303030;
+  static const ConversationTitleColor = 0xff353535;
+}
+
+class Constants {
+  static const IconFontFamily = "appIconFont";
+}
